3d0107de826a5a568e6066153ce1e0472098ad1f
Steffen Larson <steffman13@gmail.com>
2021-02-04 16:45:30 -0700

    join/create possibly work

--- questseeker.client/src/services/QuestService.js
+++ questseeker.client/src/services/QuestService.js
@@ -4,6 +4,11 @@ import { codeGenerator } from '../utils/CodeGenerator'
 import { api } from './AxiosService'
 
 class QuestService {
+  async getQuests() {
+    const res = await api.get('api/quests/')
+    AppState.quest = res.data
+  }
+
   async createQuest(newQuest) {
     newQuest.accessCode = codeGenerator(5)
     const res = await api.post('api/quests/', newQuest)
@@ -13,7 +18,16 @@ class QuestService {
   }
 
   async joinQuest(inputCode) {
-    // Make onMounted to get all quests. To filter next line
+    // filter
+    const matchingAccessCode = AppState.quests.filter(quest => quest.accessCode === inputCode.accessCode)
+    const relationshipInfo = { questId: matchingAccessCode[0].id }
+
+    // TODO test with and without await
+    await api.post('api/userquests', relationshipInfo)
+    AppState.activeQuest = matchingAccessCode[0]
+    router.push({ name: 'MapPage', params: { questId: matchingAccessCode[0].id } })
+    // ANCHOR Make onMounted to get all quests. To filter next line
+    // Route will equal api/userquests
     // TODO filter through the quests array and find where the access codes from both quests and submitted access code match.
     // Make relationship between individual and quest. (Pushing user and quest into many to many {userquest})
     // Set as active quest for user in AppState.
