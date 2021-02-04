d1445a2c7bd087de62e7d0545863ad212d5891e0
Steffen Larson <steffman13@gmail.com>
2021-02-04 13:26:00 -0700

    Router finished removed a page

--- questseeker.client/src/router.js
+++ questseeker.client/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { authGuard } from '@bcwdev/auth0provider-client'
 
+const questId = 'quests/:questid'
 function loadPage(page) {
   return () => import(`./pages/${page}.vue`)
 }
@@ -20,6 +21,42 @@ const routes = [
     path: '/account',
     name: 'Account',
     component: loadPage('AccountPage'),
+    beforeEnter: authGuard
+  },
+  {
+    path: questId + '/map',
+    name: 'Map',
+    component: loadPage('MapPage'),
+    beforeEnter: authGuard
+  },
+  {
+    path: questId + '/objectivedetails/:id',
+    name: 'ObjectiveDetails',
+    component: loadPage('ObjectiveDetailsPage'),
+    beforeEnter: authGuard
+  },
+  {
+    path: questId + '/addlocationpage',
+    name: 'AddLocationPage',
+    component: loadPage('AddLocationPage'),
+    beforeEnter: authGuard
+  },
+  {
+    path: questId + '/objectivereviewpage',
+    name: 'ObjectiveReviewPage',
+    component: loadPage('ObjectiveReviewPagePage'),
+    beforeEnter: authGuard
+  },
+  {
+    path: questId + '/teamspage',
+    name: 'TeamsPage',
+    component: loadPage('TeamsPagePage'),
+    beforeEnter: authGuard
+  },
+  {
+    path: questId + '/answerspage/:id',
+    name: 'AnswersPage',
+    component: loadPage('AnswersPage'),
     beforeEnter: authGuard
   }
 ]
