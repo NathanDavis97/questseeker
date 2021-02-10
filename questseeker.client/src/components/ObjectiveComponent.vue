d3d89882508cb857e10dc493de2e68aabe3826be
Nathandavis97 <davislnathan9@gmail.com>
2021-02-10 11:25:00 -0700

    added address

--- questseeker.client/src/components/ObjectiveComponent.vue
+++ questseeker.client/src/components/ObjectiveComponent.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="ObjectiveComponent">
-    <p>This is a button button</p>
+    <div class="row justify-content-center">
+      <div class="col-8 m-3">
+        <div class="card  text-center">
+          <strong>{{ objectiveProp.title }}</strong>
+          <p>{{ objectiveProp.location.address }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ObjectiveComponent',
+  props: {
+    objectiveProp: { type: Object, required: true }
+  },
   setup() {
     return {}
   },
--- questseeker.client/src/services/ObjectiveService.js
+++ questseeker.client/src/services/ObjectiveService.js
@@ -1,5 +1,14 @@
+import { AppState } from '../AppState'
+import { api } from './AxiosService'
 
 class ObjectiveService {
+  async getAllObjectives(id) {
+    try {
+      const res = await api.get('api/quests/' + id + '/objectives')
+      AppState.objectives = res.data
+    } catch (error) {
 
+    }
+  }
 }
 export const objectiveService = new ObjectiveService()
