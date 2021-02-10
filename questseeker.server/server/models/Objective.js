eccdf9b609463ff0a33c295d03d6d666681602f5
Kris Jones <kristop.jones@gmail.com>
2021-02-10 11:09:54 -0700

    address in schema objective

--- questseeker.server/server/models/Objective.js
+++ questseeker.server/server/models/Objective.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const Objective = new Schema(
   {
     location: {
+      address: { type: String, required: true },
       lat: { type: Number, required: true },
       lng: { type: Number, required: true }
     },
