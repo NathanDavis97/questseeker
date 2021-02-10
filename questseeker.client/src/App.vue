14eebcb60086cedfd572169e6de4bd49865cc050
Kris Jones <kristop.jones@gmail.com>
2021-02-10 13:40:27 -0700

    footer color

--- questseeker.client/src/App.vue
+++ questseeker.client/src/App.vue
@@ -10,8 +10,8 @@
     </router-view>
   </main>
   <footer>
-    <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
+    <div class="foot-bg text-light text-center p-4">
+      Made by Justin, Nathan, Steffan, Kris
     </div>
   </footer>
 </template>
@@ -30,5 +30,8 @@ export default {
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
-
+.foot-bg{
+  background-color: #2D4473 !important;
+  font-family: 'Montserrat Alternative';
+}
 </style>
