72f55ac209c03548d1c8b72723e6f5781c233100
Steffen Larson <steffman13@gmail.com>
2021-02-11 14:56:11 -0700

    working on submitting answers and hiding form

--- questseeker.client/src/services/ObjectiveService.js
+++ questseeker.client/src/services/ObjectiveService.js
@@ -11,5 +11,27 @@ class ObjectiveService {
       logger.error(error)
     }
   }
+
+  async getObjective(objective) {
+    const res = await api.get('api/objectives/' + objective.id)
+    AppState.activeObjective = res.data
+  }
+
+  async submitAnswer(answer) {
+    // TODO do a check to see if answer has already been submitted by this user.
+    await api.post('api/answers', answer)
+    logger.log(answer)
+  }
+
+  async getAnswers(objective, account) {
+    const res = await api.get('api/obejctives/' + objective.id + '/answers')
+    const filt = res.data.filter(answer => answer.creator.id === account)
+
+    if (filt.length > 0) {
+      AppState.status = true
+    } else {
+      AppState.status = false
+    }
+  }
 }
 export const objectiveService = new ObjectiveService()
